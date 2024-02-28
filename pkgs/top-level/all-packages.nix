599af097efac553f8a4c050bc7d48326d3374e7e
Felix Buehler <account@buehler.rocks>
2024-02-28 23:51:24 +0100

    moneyplex: remove
    unmaintained for years, never updated and the download no longer works.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10895,8 +10895,6 @@ with pkgs;
 
   molotov = callPackage ../applications/video/molotov { };
 
-  moneyplex = callPackage ../applications/office/moneyplex { };
-
   monit = callPackage ../tools/system/monit { };
 
   monocraft = callPackage ../data/fonts/monocraft { };
